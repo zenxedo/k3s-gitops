5fcdf8da72a982ba472badd399826eeab4e28f82
Stephen Cunningham <mow4cash@gmail.com>
2020-10-28 12:59:19 -0400

    Update bootstrap-cluster.sh

--- bootstrap-cluster.sh
+++ bootstrap-cluster.sh
@@ -2,17 +2,8 @@
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-need() {
-    which "$1" &>/dev/null || die "Binary '$1' is missing but required"
-}
-
-need "kubectl"
-need "helm"
-
 message() {
-  echo -e "\n######################################################################"
-  echo "# $1"
-  echo "######################################################################"
+  echo Hey
 }
 
 

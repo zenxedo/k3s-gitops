7ca3d6df65c10e85905f96f4dc813987300f6512
xenxedo <mow4cash@gmail.com>
2020-10-29 14:57:34 -0400

    fdg

--- bootstrap-cluster.sh
+++ bootstrap-cluster.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-echo "Hello World";
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+need() {
+    which "$1" &>/dev/null || die "Binary '$1' is missing but required"
+}
+
+need "kubectl"
+need "helm"
+
+message() {
+  echo -e "\n######################################################################"
+  echo "# $1"
+  echo "######################################################################"
+}
+
+installFlux() {
+  message "installing flux"
+  # install flux
+  helm repo add fluxcd https://charts.fluxcd.io
+  helm upgrade --install flux --values "$REPO_ROOT"/flux/flux/flux-values.yaml --namespace flux fluxcd/flux
+  helm upgrade --install helm-operator --values "$REPO_ROOT"/flux/helm-operator/flux-helm-operator-values.yaml --namespace flux fluxcd/helm-operator
+
+  FLUX_READY=1
+  while [ $FLUX_READY != 0 ]; do
+    echo "waiting for flux pod to be fully ready..."
+    kubectl -n flux wait --for condition=available deployment/flux
+    FLUX_READY="$?"
+    sleep 5
+  done
+
+message "all done!"
+kubectl get nodes -o=wide
